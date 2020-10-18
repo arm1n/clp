a11b95aec75719754965e855e23c0a96c4bb9b93
Armin Pfurtscheller <armin.pfurtscheller@cropster.org>
2020-10-18 21:27:36 +0200

    Add engine-wiring page in engine app

--- apps/engine/src/app.jsx
+++ apps/engine/src/app.jsx
@@ -4,9 +4,10 @@ import { System, Page, PAGE_TYPE_POPUP } from "@clp/shared";
 import { Engine } from "pages/engine/component";
 import { Apu } from "pages/apu/component";
 import { EngineChange } from "pages/engine-change/component";
+import { EngingWiring } from "pages/engine-wiring/component";
 import { ArcduReadout } from "pages/arcdu-readout/component";
+
 import { DrainMast } from "pages/drain-mast/component";
-import { EngWiringHarness } from "pages/eng-wiring-harness/component";
 import { CautionLights } from "pages/caution-lights/component";
 
 export const App = (props) => (
@@ -23,15 +24,16 @@ export const App = (props) => (
       <EngineChange />
     </Page>
 
+    <Page path="/engine-wiring" name="Eng Wiring">
+      <EngingWiring />
+    </Page>
+
     <Page path="/arcdu-readout" name="Arcdu Readout">
       <ArcduReadout />
     </Page>
 
     <Page name="Drain Mast" type={PAGE_TYPE_POPUP} path="/drain-mast">
       <DrainMast />
-    </Page>
-    <Page name="Eng Wiring Harness" type={PAGE_TYPE_POPUP} path="/eng-wiring-harness">
-      <EngWiringHarness />
     </Page>
     <Page name="Caution Lights" type={PAGE_TYPE_POPUP} path="/caution-lights">
       <CautionLights />
--- apps/engine/src/pages/eng-wiring-harness/component.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { File } from "@clp/shared";
-import { HTML_PATH } from "config/eng-wiring-harness/config";
-
-// import styles from "./component.module.scss";
-
-export const EngWiringHarness = () => <File path={HTML_PATH} />;
