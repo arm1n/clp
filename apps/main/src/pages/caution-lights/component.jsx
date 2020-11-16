67aae1dfe06da545a5713a13caff5c75afa28f36
Armin Pfurtscheller <armin.pfurtscheller@cropster.org>
2020-11-16 21:52:09 +0100

    Add 'show_display_fault_msg' option for caution lights

--- apps/main/src/pages/caution-lights/component.jsx
+++ apps/main/src/pages/caution-lights/component.jsx
@@ -4,6 +4,7 @@ import { useRoute, getProperty } from "@clp/shared";
 
 import { CAUTION_LIGHTS } from "config/dashboard/config";
 import { CautionLightsSearch } from "components/caution-lights-search";
+import { DisplayFaultMsgSearch } from "components/display-fault-msg-search";
 import { EngineApuEcsFaultsSearch } from "components/engine-apu-ecs-faults-search";
 import { SubPage } from "components/sub-page";
 
@@ -15,14 +16,24 @@ export const CautionLights = () => {
   } = useRoute();
 
   // note: `row` is NOT zero-based when extracted from URL, thus we have to deduct 1
-  const { name = null, file = null, buttons = [], show_eng_apu_ecs_search = false } =
-    getProperty(CAUTION_LIGHTS.rows, `${row - 1}.${col}`) || {};
+  const {
+    name = null,
+    file = null,
+    buttons = [],
+    show_display_fault_msg = false,
+    show_eng_apu_ecs_search = false,
+  } = getProperty(CAUTION_LIGHTS.rows, `${row - 1}.${col}`) || {};
 
   const search = (
     <div className={styles.wrapper}>
       <div className={styles.search}>
         <CautionLightsSearch />
       </div>
+      {show_display_fault_msg ? (
+        <div className={styles.search}>
+          <DisplayFaultMsgSearch />
+        </div>
+      ) : null}
       {show_eng_apu_ecs_search ? (
         <div className={styles.search}>
           <EngineApuEcsFaultsSearch />
